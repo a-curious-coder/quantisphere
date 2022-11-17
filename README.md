2536333dba397fa96f0d7d3d111643d1ecad84fb
zardoss <callummclennan1@googlemail.com>
2022-11-17 11:18:54 +0000

    Under reconstruction

--- README.md
+++ README.md
@@ -1,40 +1,23 @@
-# Crypto Analayst
-This project scrapes price data from CoinMarketCap to predict the future price over a user-specified number of days. This project uses a Long Short Term Memory Neural Network that works with time series price data.
+# Crypto Analyst
+Price data is scraped from CoinMarketCap, processed and used to predict the future price of a user-specified crypto currency over a set number of days. This project uses Deep Learning, incorporating LSTM layers in the neural network to best analyse and predict new prices according to the time series price data.
+
+![Image](media/crypto_landscape.jpg)
+
+
 # Setup
 
-First, we need to set up a virtual environment. If you do not have 'virtualenv' installed on your system, follow the steps below corresponding to your operating system.
+Create your virtual environment and install the libraries from requirements.txt:
 
-```bash
-# Windows and Mac
-pip install virtualenv
-```
+## Windows
+    $ python virtualenv .venv
+    $ .venv\Scripts\activate
+    $ pip install -r requirements.txt
 
-With virtualenv installed, we need to create the virtual environment.
+## Linux / Mac
 
-```bash
-# Windows and Mac
-virtualenv <virtual environment name>
-```
+    $ python virtualenv .venv
+    $ source .venv/bin/activate
+    $ pip install -r requirements.txt
 
-The virtual environment is created. We need to initialise the virtual environment in order to install all of the preliminary requirements to run this project.
-
-```bash
-# Windows
-<virtual environment name>\Scripts\activate.bat
-
-# Mac
-source <virtual environment name>/bin/activate
-```
-
-With the virtual environment activated, we need to install the requirements from the 'requirements.txt' file
-
-```bash
-# Windows and Mac
-pip install -r requirements.txt
-```
-
-Once all of the requirements are installed, run 'main.py'
-
-```bash
-python main.py
-```
+# Methodology
+Once data was collected, I had to figure out the best way to split the time-series data into train/test sets. I decided to use the last 20% of the data as the test set, and the remaining 80% as the training set. This is because it gives the model the most recent data to train on, and the most recent data to test on. This is the most accurate way to test the model, as it is the most recent data that we are trying to predict.
--- main.py
+++ main.py
@@ -1,245 +1,13 @@
-#!/usr/bin/env python
-
 import os
-from os.path import exists
+import sys
 
 import numpy as np
 import pandas as pd
 from cryptocmd import CmcScraper
 from currency_converter import CurrencyConverter
-from dotenv import load_dotenv
-from matplotlib import pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.layers import LSTM, Dense, Dropout
-from tensorflow.keras.models import Sequential, load_model
 
-
-def get_crypto_data():
-    """ Get crypto currency data from CoinMarketCap 
-    
-    Returns:
-        pandas.DataFrame -- DataFrame containing crypto currency data
-    """
-    if exists(f"{CRYPTO_CURRENCY}_all_time.csv"):
-        print(f"[*]\tLoading {CRYPTO_CURRENCY} data from file")
-        return pd.read_csv(f"{CRYPTO_CURRENCY}_all_time.csv")
-    print("[*]\t Getting crypto data")
-    # initialise scraper without time interval
-    scraper = CmcScraper(CRYPTO_CURRENCY)
-
-    # get raw data as list of list
-    headers, data = scraper.get_data()
-
-    # get data in a json format
-    xrp_json_data = scraper.get_data("json")
-
-    # export the data as csv file, you can also pass optional `name` parameter
-    scraper.export("csv", name=f"{CRYPTO_CURRENCY}_all_time")
-
-    # Pandas dataFrame for the same data
-    df = scraper.get_dataframe()
-
-    # High, Low, Open, Close, Volume, Adj Close
-    data_frame = pd.DataFrame(data=df, columns = ['Date', 'High', 'Low', 'Open', 'Close', 'Volume'])
-    data_frame['Close'] = data_frame['Close'].apply(convert_to_gbp)
-    return data_frame
-
-
-def convert_to_gbp(value):
-    """ Convert crypto currency value to GBP 
-    
-    Arguments:
-        value {float} -- Crypto currency value
-    
-    Returns:
-        float -- Converted value
-    """
-    # Initialise currency converter
-    c = CurrencyConverter()
-    # Convert to GBP
-    value = c.convert(float(value), 'USD', 'GBP')
-    # Return GBP value
-    return value
-
-
-def plot_actual_vs_prediction(actual, pred):
-    """ Plot the predictions
-    
-    Arguments:
-        actual {np.array} -- Actual values
-        pred {np.array} -- Predicted values
-    """
-    print(f"[*]\tActual:\n{actual[-5:]}")
-    print(f"[*]\tPredicted:\n{pred[-5:]}")
-    plt.plot(actual[:], color = 'black', label='Actual Prices')
-    plt.plot(pred[:], color='green', label = 'Predicted Prices')
-    plt.title(f"{CRYPTO_CURRENCY} price prediction")
-    plt.xlabel('Time')
-    plt.ylabel('Price')
-    plt.legend(loc = 'upper left')
-    plt.savefig('actual_vs_prediction_all_time.png', bbox_inches='tight')
-
-
-def plot_prediction(pred):
-    """ Plot the prediction
-    
-    Arguments:
-        pred {np.array} -- Predicted values
-    """
-    print("[*]\tPlotting prediction")
-    plt.figure()
-    plt.plot(pred, color = 'orange', label = 'Predicted Prices')
-    plt.title(f"{CRYPTO_CURRENCY} price prediction")
-    plt.xlabel('Time')
-    plt.ylabel('Price')
-    plt.legend(loc = 'upper left')
-    plt.savefig(f'prediction_next_{PREDICTION_DAYS}_days.png', bbox_inches='tight')
-
-
-def prepare_training_data(scaled_data):
-    """ Prepare training data for the model
-    
-    Arguments:
-        scaled_data {np.array} -- Scaled data
-    
-    Returns:
-        x_train {np.array} -- Training data
-        y_train {np.array} -- Training labels
-    """
-    print("[*]\tPreparing training data")
-    # Prepare training data - Supervised learning
-    x_train, y_train = [], []
-    
-    for x in range(PREDICTION_DAYS, len(scaled_data)):
-        x_train.append(scaled_data[x-PREDICTION_DAYS:x, 0])
-        y_train.append(scaled_data[x, 0])
-
-    # Convert to np arrays
-    x_train, y_train = np.array(x_train), np.array(y_train)
-    # Reshape training
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    print("Training data prepared")
-    return x_train, y_train
-
-
-def create_model(x_train):
-    """ Create a model with LSTM layers and dropout 
-    
-    Arguments:
-        x_train {np.array} -- Training data
-        y_train {np.array} -- Training labels
-    
-    Returns:
-        model {keras.model} -- Model
-    """
-    print("[*]\tCreating predictive model")
-    # Create Neural Network
-    model = Sequential()
-
-    # LSTM layers - recurrent layers to memorise stuff from each day - specialised on this sort of data - units = nodes
-    model.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], 1)))
-    model.add(Dropout(0.2)) # Prevent overfitting
-    model.add(LSTM(units=50, return_sequences = True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units = 50))
-    model.add(Dropout(0.2))
-    model.add(Dense(units=1))
-    return model
-
-
-def train_model(x_train, y_train, model):
-    """ Train the model
-        
-    Arguments:
-        x_train {np.array} -- Training data
-        y_train {np.array} -- Training labels
-        model {keras.model} -- Model
-
-    Returns:
-        model {keras.model} -- Trained model
-    """
-    print("[*]\tTraining model")
-    # Compile model
-    model.compile(optimizer = 'adam', loss = 'mean_squared_error')
-    # Train model
-    model.fit(x_train, y_train, batch_size = 32, epochs = 10)
-    # Save model
-    model.save(f'{CRYPTO_CURRENCY}_model.h5')
-    print("[*]\tModel trained")
-    return model
-
-
-def evaluate_model(data, scaler, model):
-    """ Evaluate the model by plotting actual and predicted prices
-    
-    Arguments:
-        PREDICTION_DAYS {int} -- Number of days to predict
-        data {pandas.DataFrame} -- DataFrame containing the data
-        scaler {sklearn.preprocessing.MinMaxScaler} -- Scaler
-        model {keras.model} -- Model
-    """
-    print("[*]\tEvaluating model")
-    # Testing model
-    test_data = get_crypto_data()
-
-    actual_prices = test_data['Close'].values
-
-    total_dataset = pd.concat((data['Close'], test_data['Close']), axis = 0)
-
-    model_inputs = total_dataset[len(total_dataset) - len(test_data) - PREDICTION_DAYS:].values
-    model_inputs = model_inputs.reshape(-1, 1)
-    model_inputs = scaler.fit_transform(model_inputs)
-
-    # Predictions
-    x_test = [model_inputs[x - PREDICTION_DAYS : x, 0] for x in range(PREDICTION_DAYS, len(model_inputs))]
-
-
-    x_test = np.array(x_test)
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-    # Predicts prices
-    prediction_prices = model.predict(x_test)
-
-    # To get actual prices values from 0-1 values
-    prediction_prices = scaler.inverse_transform(prediction_prices)
-    plot_actual_vs_prediction(actual_prices, prediction_prices)
-
-
-def predict(data, model, scaler):
-    """ Predict the price for the next PREDICTION_DAYS
-    
-    Arguments:
-        data {pandas.DataFrame} -- DataFrame containing the data
-        model {keras.model} -- Model
-        scaler {sklearn.preprocessing.MinMaxScaler} -- Scaler
-        
-    Returns:
-        float -- Predicted price
-    """
-    print(f"[*]\tPredicting tomorrow's price for {CRYPTO_CURRENCY}")
-    test_data = get_crypto_data()
-
-    total_dataset = pd.concat((data['Close'], test_data['Close']), axis = 0)
-
-    model_inputs = total_dataset[len(total_dataset) - len(test_data) - PREDICTION_DAYS:].values
-    model_inputs = model_inputs.reshape(-1, 1)
-    model_inputs = scaler.fit_transform(model_inputs)
-    # Predicting next day
-    real_data = [model_inputs[len(model_inputs) + 1 - PREDICTION_DAYS: len(model_inputs) + 1, 0]]
-    real_data = np.array(real_data)
-    real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
-
-    preds = model.predict(real_data)
-    print(len(preds))
-    print(preds)
-    input()
-    # To get actual prices values from 0-1 values
-    preds = scaler.inverse_transform(preds)
-
-    plot_prediction(preds)
-
-    final = format(float(preds[0]), '.8f') if preds[0] < 0.001 else float(preds[0])
-    print(f"Tomorrow's price for {CRYPTO_CURRENCY} is £{final}")
+from crypto_analyst import CryptoAnalyst
 
 
 def cls():
@@ -247,31 +15,148 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
 
-def main():
-    load_dotenv()
-    global CRYPTO_CURRENCY
-    global CURRENCY
-    global PREDICTION_DAYS
+def convert_to_gbp(value):
+    """ Convert crypto currency value to GBP
+    Parameters
+    ----------
+    value : float
+        Crypto currency value
+    Returns
+    -------
+    value : float
+        Crypto currency value in GBP
+    """
+    converter = CurrencyConverter()
+    # Convert to GBP
+    value = converter.convert(float(value), 'USD', 'GBP')
+    return value
 
-    CRYPTO_CURRENCY = os.getenv('CRYPTO_CURRENCY')
-    CURRENCY = os.getenv('CURRENCY')
-    PREDICTION_DAYS = int(os.getenv('PREDICTION_DAYS'))
+
+def get_crypto_data(crypto_currency):
+    """ Get crypto currency data from CoinMarketCap
+    Returns
+    -------
+    crypto_data : pandas.DataFrame
+        Dataframe with crypto currency data
+    """
+    # If the file already exists
+    if os.path.exists(f"{crypto_currency}_all_time.csv"):
+        print(f"[INFO]\tLoading {crypto_currency} data from file")
+        return pd.read_csv(f"{crypto_currency}_all_time.csv")
+
+    print("[INFO]\t Getting crypto data")
+    # initialise scraper without time interval
+    scraper = CmcScraper(crypto_currency)
+
+    # export the data as csv file, you can also pass optional `name` parameter
+    # scraper.export("csv", name=f"{crypto_currency}_all_time")
+
+    # Pandas dataFrame for the same data
+    crypto_data = scraper.get_dataframe()
+
+    # Set headers for data
+    crypto_data = pd.DataFrame(
+        data=crypto_data,
+        columns = [
+            'Date',
+            'High',
+            'Low',
+            'Open',
+            'Close',
+            'Volume'
+        ]
+    )
+    crypto_data.to_csv(f"{crypto_currency}_all_time.csv", index=False)
+    crypto_data['Open'] = crypto_data['Open'].apply(convert_to_gbp)
+    crypto_data['High'] = crypto_data['High'].apply(convert_to_gbp)
+    crypto_data['Low'] = crypto_data['Low'].apply(convert_to_gbp)
+    crypto_data['Close'] = crypto_data['Close'].apply(convert_to_gbp)
+    # Save data to file
+    crypto_data.to_csv(f"{crypto_currency}_all_time.csv", index=False)
+    return crypto_data
+
+
+def get_training_data(data):
+    """ Prepare training data for the model
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        Dataframe with crypto currency data
+    Returns
+    -------
+    training_data : np.array
+        Training data for the model
+    """
+    print("[INFO]\tPreparing training data")
+    # Convert dataframe into dictionary of columns
+    data = data.to_dict('list')
+    # Get the close price
+    close_data = data['Close']
+    print(len(close_data))
+    # Create training data
+    training_data = []
+    for i in range(60, len(close_data)):
+        training_data.append(close_data[i-60:i])
+    # Convert to numpy array
+    training_data = np.array(training_data)
+    return training_data
+
+    # Prepare training data - Supervised learning
+    x_train, y_train = [], []
+    
+    for day in range(3, len(data)):
+        x_train.append(data[day-3:day, 0])
+        y_train.append(data[day, 0])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    # Reshape training
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    print("[INFO] Training data prepared")
+    return x_train, y_train
+
+
+def main():
+    """ Main function """
+    crypto = "ada"
 
     cls()
     print("Welcome to crypto-analyst")
-    data = get_crypto_data()
-    
-    # Prepare data
+    if not os.path.exists('data.csv'):
+        # Collect data and split into training and testing
+        data = get_crypto_data(crypto)
+    else:
+        data = pd.read_csv('data.csv')
+
     # Scale data between 0 and 1
     scaler = MinMaxScaler(feature_range= (0, 1))
     # Normalise between -1 and 1
     scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+    # Split data into training and testing
+    training_data = scaled_data[:int(len(scaled_data) * 0.8)]
+    testing_data = scaled_data[int(len(scaled_data) * 0.8) - 60:]
+    
     # Gets training data
-    x_train, y_train = prepare_training_data(scaled_data)
+    x_train, y_train = get_training_data(scaled_data)
+
+    actual_prices = test_data['Close'].values
+
+    total_dataset = pd.concat((data['Close'], test_data['Close']), axis = 0)
+
+    model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+    model_inputs = model_inputs.reshape(-1, 1)
+    model_inputs = scaler.fit_transform(model_inputs)
+
+    # Predictions
+    x_test = [model_inputs[x - prediction_days : x, 0] for x in range(prediction_days, len(model_inputs))]
+
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    
+    crypto_analyst = CryptoAnalyst(crypto, 3, x_train, y_train, x_test, y_test)
     # If a trained model already exists
-    if os.path.exists(f'{CRYPTO_CURRENCY}_model.h5'):
-        print("[*]\tLoading model")
-        model = load_model(f'{CRYPTO_CURRENCY}_model.h5')
+    if os.path.exists(f'{crypto}_model.h5'):
+        print("[INFO]\tLoading model")
+        model = load_model(f'{crypto}_model.h5')
     else:
         # Create model
         model = create_model(x_train)
@@ -280,9 +165,12 @@ def main():
     # Evaluate model
     evaluate_model(data, scaler, model)
     # Predict future prices
-    predict(data, model, scaler)
-    
+    test_model(data, model, scaler)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Exiting...")
+        sys.exit(0)
